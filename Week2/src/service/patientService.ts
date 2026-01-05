7d42c6c42ba5bd514ab580974a43da7a40f1ef6c
viet30701via <viet30701.via@gmail.com>
2026-01-05 10:19:23 +0700

    Save change

--- Week2/src/service/patientService.ts
+++ Week2/src/service/patientService.ts
@@ -2,18 +2,18 @@ import { Patient } from "../Models/patient";
 import { IRepository } from "../Repository/repository";
 import { validatePatient } from "../Validate/validatePatient";
 
-export class PatientService{
-    constructor(private repository : IRepository<Patient>){}
-    addPatient(patient : Patient) : void {
-        validatePatient(patient);
-        this.repository.add(patient);
-        console.log(`Patient name : ${patient.name} has been added`);
-    }
-    
-    // private validate(patient : Patient) : void {
-    //     if(patient.age < 0 || patient.age > 100)
-    //     {
-    //         throw new Error("Invalid age");
-    //     }
-    // }
+export class PatientService {
+  constructor(private repository: IRepository<Patient>) {}
+  addPatient(patient: Patient): void {
+    validatePatient(patient);
+    this.repository.add(patient);
+    console.log(`Patient name : ${patient.name} has been added`);
+  }
+
+  // private validate(patient : Patient) : void {
+  //     if(patient.age < 0 || patient.age > 100)
+  //     {
+  //         throw new Error("Invalid age");
+  //     }
+  // }
 }
