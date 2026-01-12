c0b80c04cd47e36d9cd4a41cf71b79f583bd317f
viet30701via <viet30701.via@gmail.com>
2026-01-12 16:45:28 +0700

    fix environment variable

--- Week4/emr-nextjs/src/app/(dashboard)/patients/page.tsx
+++ Week4/emr-nextjs/src/app/(dashboard)/patients/page.tsx
@@ -1,19 +1,31 @@
 import PatientList from "@/components/patients/PatientList";
-export const generateMetadata = {
+
+export const metadata = {
   title: "EMR - Quản lý Bệnh nhân",
   description: "Danh sách và quản lý thông tin bệnh nhân trong hệ thống EMR",
 };
-//SSR
+
 async function getPatients() {
-  const res = await fetch("http:localhost:3000/patients.json", {
-    cache: "no-store",
-  });
-  if (!res.ok) throw new Error("Failed to fetch patients");
-  return res.json();
+  try {
+    const apiUrl =
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+
+    const res = await fetch(`${baseUrl}/patients.json`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) return [];
+    return res.json();
+  } catch (error) {
+    return [];
+  }
 }
 
 export default async function PatientsPage() {
   const initialPatients = await getPatients();
+
   return (
     <div className="bg-white rounded-xl shadow-lg p-6">
       <PatientList initialData={initialPatients} />
